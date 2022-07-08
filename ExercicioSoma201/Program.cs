f4c1b37e7e75b45171955355b25c73274c90f644
Lucas-Reluz <lucas.pokemon.61@gmail.com>
2022-07-08 18:33:11 -0300

    @Lucas-Reluz [Task: Feito teste simples onde a soma sempre é 201] {18:33 | 08/07/2022}

---
+++ ExercicioSoma201/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TestesC_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numb2 = 0;
+
+            for (int n1 = 200; n1 > numb2; n1--)
+            {
+                numb2++;
+                int numero = n1 + numb2;
+                Console.WriteLine($"{n1} + {numb2} = {numero}");
+            }
+        }
+    }
+}
